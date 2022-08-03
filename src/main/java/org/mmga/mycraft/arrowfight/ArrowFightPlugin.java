4079787137b02175f5e66bdc0aac8a7fd291deca
Wzp-2008 <w433200w>
2022-08-03 16:59:46 +0800

    First Commit

---
+++ src/main/java/org/mmga/mycraft/arrowfight/ArrowFightPlugin.java
@@ -0,0 +1,23 @@
+package org.mmga.mycraft.arrowfight;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created on 2022/8/3 16:56:17
+ *
+ * @author wzp
+ * @version 1.0.0
+ */
+
+public final class ArrowFightPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
